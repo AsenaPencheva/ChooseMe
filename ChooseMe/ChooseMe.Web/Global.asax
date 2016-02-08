84ccd8eef5c2e54aff8e520778be6986651d04aa
PavDragneva <pavlinadragneva@yahoo.com>
2016-02-08 15:32:57 +0200

    Added Animal Model

---
+++ ChooseMe/ChooseMe.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChooseMe.Web.MvcApplication" Language="C#" %>
---
+++ ChooseMe/ChooseMe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChooseMe.Web.Startup))]
+namespace ChooseMe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
