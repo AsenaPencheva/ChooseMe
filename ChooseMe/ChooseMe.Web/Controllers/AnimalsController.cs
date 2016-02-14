846c225787c2d8b34262f87050c4ddf0128e0727
PavDragneva <pavlinadragneva@yahoo.com>
2016-02-14 05:26:48 +0200

    Added list view for all animals

---
+++ ChooseMe/ChooseMe.Web/Controllers/AnimalsController.cs
@@ -0,0 +1,20 @@
+﻿namespace ChooseMe.Web.Controllers
+{
+    using AutoMapper.QueryableExtensions;
+    using Models.Animal;
+    using Ninject;
+    using Services.Contracts;
+    using System.Web.Mvc;
+
+    public class AnimalsController:Controller
+    {
+        [Inject]
+        public IAnimalService AnimalService { get; set; }
+
+        public ActionResult All()
+        {
+            var animals = AnimalService.GetAll().ProjectTo<AnimalsListView>();
+            return View(animals);
+        }
+    }
+}
