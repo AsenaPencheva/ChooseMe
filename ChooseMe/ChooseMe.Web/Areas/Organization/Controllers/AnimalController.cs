5914d1eb89de82d28087fdaaa741a44114c98097
PavDragneva <pavlinadragneva@yahoo.com>
2016-02-18 11:30:35 +0200

    Added create animal

---
+++ ChooseMe/ChooseMe.Web/Areas/Organization/Controllers/AnimalController.cs
@@ -0,0 +1,82 @@
+﻿namespace ChooseMe.Web.Areas.Organization.Controllers
+{
+    using ChooseMe.Models;
+    using Microsoft.AspNet.Identity;
+    using Models.Animal;
+    using Services.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class AnimalController:Controller
+    {
+        private IAnimalService animals;
+        private IOrganizationService organizations;
+
+        public AnimalController(IAnimalService animals, IOrganizationService organizations)
+        {
+            this.animals = animals;
+            this.organizations = organizations;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                var newAnimal = AutoMapper.Mapper.Map<Animal>(model);
+
+                newAnimal.OrganizationId = this.User.Identity.GetUserId();
+
+                newAnimal.Organization = organizations.GetById(newAnimal.OrganizationId).FirstOrDefault();
+
+                newAnimal.AddedOn = DateTime.UtcNow;
+
+                var result = this.animals.AddNew(newAnimal);
+
+                return this.RedirectToAction("Upload", "Animal", new { area = "Organization", id = result.Id, animal = newAnimal });
+            }
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(CreateViewModel model, int id, Animal animal)
+        {
+            foreach (var file in model.Files)
+            {
+                if (file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    file.SaveAs(path);
+                    var photo = new Photo();
+                    photo.Address = path;
+                    photo.AnimalId = id;
+                    photo.Animal = animal;
+                    animal.Photos.Add(photo);
+
+                    return this.RedirectToAction("Details", "Animasl", new { area = "", id = id });
+                }
+            }
+            return this.View(model);
+        }
+    }
+}
---
+++ ChooseMe/ChooseMe.Web/Areas/Organization/Models/Animal/CreateViewModel.cs
@@ -0,0 +1,69 @@
+﻿namespace ChooseMe.Web.Areas.Organization.Models.Animal
+{
+    using Common.Enums;
+    using ChooseMe.Models;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using Infrastructure.Mappings;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Collections.Generic;
+    using System.Web;
+    public class CreateViewModel: IMapFrom<Animal>
+    {
+        [Required]
+        [DisplayName("Cat or Dog")]
+        public AnimalType Type { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "The name must be at least 3 characters long!")]
+        public string Name { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        public DateTime AddedOn { get; set; }
+
+        [Required]
+        [DisplayName("Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+
+        public string Story { get; set; }
+
+        public string Disease { get; set; }
+
+        [Required]
+        [DisplayName("Kids friendly")]
+        public bool IsKidsFriendly { get; set; }
+
+        [Required]
+        [DisplayName("Dogs friendly")]
+        public bool IsDogsFriendly { get; set; }
+
+        [Required]
+        [DisplayName("Cats friendly")]
+        public bool IsCatsFriendly { get; set; }
+
+        [Required]
+        [DisplayName("Color")]
+        public FurColor FurColor { get; set; }
+
+        [Required]
+        [DisplayName("Longhaired")]
+        public bool IsLonghaired { get; set; }
+
+        [Required]
+        [DisplayName("Castraited")]
+        public bool IsCastraited { get; set; }
+
+        [Required]
+        [DisplayName("Vaccinated")]
+        public bool IsVaccinated { get; set; }
+
+        [Required]
+        [DisplayName("Chipped")]
+        public bool IsChipped { get; set; }
+
+        public IEnumerable<HttpPostedFileBase> Files { get; set; }
+    }
+}
