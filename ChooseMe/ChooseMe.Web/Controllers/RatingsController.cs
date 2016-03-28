629b0077a0978049ac976e7751de242ee85056bf
PavDragneva <pavlinadragneva@yahoo.com>
2016-03-29 02:12:16 +0300

    Added Average rating and Your rating in Organization Details

--- ChooseMe/ChooseMe.Web/Controllers/RatingsController.cs
+++ ChooseMe/ChooseMe.Web/Controllers/RatingsController.cs
@@ -6,6 +6,7 @@
     using Models.Rating;
     using Services.Contracts;
     using System;
+    using System.Linq;
     using System.Web.Mvc;
 
     public class RatingsController : Controller
@@ -27,18 +28,11 @@
 
                 model.UserId = User.Identity.GetUserId();
 
-                if (this.ratings.CheckIfRate(model.UserId))
-                {
-                    this.ratings.Update(model.Value, model.UserId);
-                }
-                else
-                {
                     model.OrganizationId = model.OrganizationId;
 
                     var newRating = Mapper.Map<Rating>(model);
 
                     this.ratings.AddNew(newRating);
-                }
 
                 return this.RedirectToAction("Details", "Organizations", new { area = "", id =  model.OrganizationId});
             }
@@ -50,6 +44,26 @@
         public ActionResult Average(string id)
         {
             return this.Content(this.ratings.GetAverage(id).ToString());
+        }
+
+        [HttpGet]
+        public ActionResult CheckIfRate(string id)
+        {
+            if(this.ratings.CheckIfRate(id))
+            {
+                return Content("true");
+            }
+            else
+            {
+                return Content("false");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult UserRating(string userId, string orgId)
+        {
+            var rating = this.ratings.GetByUserAndOrganizationId(userId, orgId).FirstOrDefault();
+            return Content(rating.Value.ToString());
         }
     }
 }
